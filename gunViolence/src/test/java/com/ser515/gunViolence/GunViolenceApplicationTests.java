2dd5ee1b80100f3b2674580b073a348a50c61c63
sambhavv14asu <skapoo26@asu.edu>
2022-11-22 01:13:20 -0700

    initial commit springboot

---
+++ gunViolence/src/test/java/com/ser515/gunViolence/GunViolenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ser515.gunViolence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GunViolenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
